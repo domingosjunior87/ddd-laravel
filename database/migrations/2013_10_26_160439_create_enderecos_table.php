7a63b0f884e4153a08767273d9d71bb2b842b47a
domingos.coelho <domingos@prodam.am.gov.br>
2021-10-26 14:30:11 -0400

    Ajustando migrations

---
+++ database/migrations/2013_10_26_160439_create_enderecos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnderecosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enderecos', function (Blueprint $table) {
+            $table->id();
+            $table->string('identificacao');
+            $table->string('cep', 5);
+            $table->string('logradouro');
+            $table->string('numero');
+            $table->string('complemento')->nullable();
+            $table->string('bairro');
+            $table->string('cidade');
+            $table->string('referencia')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enderecos');
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,20 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('nome');
+            $table->timestamp('data_nascimento');
+            $table->string('sexo')->nullable();
+            $table->string('cpf', 11)->unique();
+            $table->string('rg')->nullable();
+            $table->integer('endereco_id');
+            $table->integer('telefone')->nullable();
+            $table->integer('celular');
+            $table->string('email', 200)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->foreign('endereco_id')->references('id')->on('enderecos');
         });
     }
 
